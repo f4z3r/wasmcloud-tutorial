4e27128a76e38bd00d29f8ec1fe319bccccd978e
Jakob Beckmann <f4z3r-github@pm.me>
2025-09-27 18:12:19 +0200

    feat(kubernetes): add second component that is linked to echo
    Signed-off-by: Jakob Beckmann <f4z3r-github@pm.me>

--- kubernetes/components/echo/src/lib.rs
+++ kubernetes/components/echo/src/lib.rs
@@ -5,6 +5,8 @@ use wasmcloud::messaging::*;
 
 use wasmcloud_component::http;
 
+use crate::demo::booking_master::booking_management;
+
 struct Component;
 
 http::export!(Component);
@@ -13,29 +15,16 @@ impl http::Server for Component {
     fn handle(
         _request: http::IncomingRequest,
     ) -> http::Result<http::Response<impl http::OutgoingBody>> {
+        let booking = booking_management::get_booking(1).unwrap();
         consumer::publish(&types::BrokerMessage {
             subject: "echo.response".into(),
             reply_to: None,
-            body: "This is a test".into(),
-        }).map_err(|e| http::ErrorCode::InternalError(Some(e.into())))?;
+            body: booking.into(),
+        })
+        .map_err(|e| http::ErrorCode::InternalError(Some(e.into())))?;
 
-        Ok(http::Response::new("Published a message to echo.response subject\n"))
+        Ok(http::Response::new(
+            "Published a message to echo.response subject\n",
+        ))
     }
 }
-
-    // fn handle_message(msg: types::BrokerMessage) -> Result<(), String> {
-    //     if let Some(reply_to) = msg.reply_to {
-    //         consumer::publish(&types::BrokerMessage {
-    //             subject: reply_to,
-    //             reply_to: None,
-    //             body: msg.body,
-    //         })
-    //     } else {
-    //         log(
-    //             Level::Warn,
-    //             "",
-    //             "No reply_to field in message, ignoring message",
-    //         );
-    //         Ok(())
-    //     }
-    // }
