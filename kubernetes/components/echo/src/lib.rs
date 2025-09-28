56bd56b9ec9fca6d96beb2f21586502722f85a45
Jakob Beckmann <f4z3r-github@pm.me>
2025-09-28 13:33:18 +0200

    feat(kubernetes): add support for a local redis K/V setup
    Signed-off-by: Jakob Beckmann <f4z3r-github@pm.me>

--- kubernetes/components/echo/src/lib.rs
+++ kubernetes/components/echo/src/lib.rs
@@ -1,30 +1,95 @@
 wit_bindgen::generate!({ generate_all });
 
-// use wasi::logging::logging::*;
+use std::num::ParseIntError;
+
+use anyhow::{anyhow, Result};
+
+use wasi::logging::logging::*;
 use wasmcloud::messaging::*;
 
 use wasmcloud_component::http;
 
 use crate::demo::booking_master::booking_management;
 
+const LOG_CONTEXT: &'static str = "demo.echo";
+const PUBLISH_SUBJECT: &'static str = "bookings.events";
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct RequestBody {
+    booking: String,
+}
+
 struct Component;
 
-http::export!(Component);
+impl Component {
+    fn parse_id(path: &str) -> Result<u32> {
+        path.split("/")
+            .last()
+            .ok_or(anyhow!("Failed to parse ID from path: {path}"))?
+            .parse()
+            .map_err(|e: ParseIntError| e.into())
+    }
+}
 
 impl http::Server for Component {
     fn handle(
-        _request: http::IncomingRequest,
+        mut request: http::IncomingRequest,
     ) -> http::Result<http::Response<impl http::OutgoingBody>> {
-        let booking = booking_management::get_booking(1).unwrap();
-        consumer::publish(&types::BrokerMessage {
-            subject: "echo.response".into(),
-            reply_to: None,
-            body: booking.into(),
-        })
-        .map_err(|e| http::ErrorCode::InternalError(Some(e.into())))?;
-
-        Ok(http::Response::new(
-            "Published a message to echo.response subject\n",
-        ))
+        match request.method() {
+            &http::Method::GET => {
+                let path = request.uri().to_string();
+                let id = Self::parse_id(&path)
+                    .map_err(|e| http::ErrorCode::InternalError(Some(e.to_string())))?;
+                let booking = booking_management::get_booking(id)
+                    .map_err(|e| http::ErrorCode::InternalError(Some(e.into())))?;
+                consumer::publish(&types::BrokerMessage {
+                    subject: PUBLISH_SUBJECT.into(),
+                    reply_to: None,
+                    body: format!("Retrieved booking {id}: {booking}").into(),
+                })
+                .map_err(|e| http::ErrorCode::InternalError(Some(e.into())))?;
+                log(
+                    Level::Info,
+                    LOG_CONTEXT,
+                    &format!("handled GET request for ID {id}"),
+                );
+                Ok(http::Response::new(format!("Booking {id} created\n")))
+            }
+            &http::Method::POST => {
+                let path = request.uri().to_string();
+                let id = Self::parse_id(&path)
+                    .map_err(|e| http::ErrorCode::InternalError(Some(e.to_string())))?;
+                let subject: RequestBody = match serde_json::from_reader(request.body_mut()) {
+                    Ok(v) => v,
+                    Err(e) => {
+                        return http::Response::builder()
+                            .status(http::StatusCode::BAD_REQUEST)
+                            .body(format!("invalid booking request: {e}").into())
+                            .map_err(|e| http::ErrorCode::InternalError(Some(e.to_string())));
+                    }
+                };
+                let booking = subject.booking;
+                booking_management::add_booking(id, &booking)
+                    .map_err(|e| http::ErrorCode::InternalError(Some(e.into())))?;
+                consumer::publish(&types::BrokerMessage {
+                    subject: PUBLISH_SUBJECT.into(),
+                    reply_to: None,
+                    body: format!("Created booking {id}: {booking}").into(),
+                })
+                .map_err(|e| http::ErrorCode::InternalError(Some(e.into())))?;
+                log(
+                    Level::Info,
+                    LOG_CONTEXT,
+                    &format!("handled POST request for ID {id}"),
+                );
+                Ok(http::Response::new(format!("Booking {id}: {booking}\n")))
+            }
+            _ => {
+                // TODO(@f4z3r): support delete
+                panic!("fail");
+            }
+        }
     }
 }
+
+http::export!(Component);
